4b2b0cac7bfbc29573d559b14310a23ccf6846da
Aditya Kumar Yadav <167506647+Aditya5548@users.noreply.github.com>
2025-03-18 20:13:10 +0530

    Update Udash.jsx

--- frontend/src/Userapp/Udash.jsx
+++ frontend/src/Userapp/Udash.jsx
@@ -37,7 +37,7 @@ function Udash(){
         }
         setTimeout(() => {
             setM(<Membership/>)
-        }, 1100);
+        }, 2000);
     //Membership cheak end
 
     //calling function
